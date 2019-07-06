4bfd3cfef50238cc2753d5fc895b1890599afe6b
Chiranjib Bakchi <chiru.cu05@gmail.com>
2019-07-06 17:17:07 +0530

    first commit

---
+++ src/main/java/com/chiru/webapp/controller/LoginController.java
@@ -0,0 +1,19 @@
+package com.chiru.webapp.controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+	@Value("${welcome.message:test}")
+	private String message = "Hello World";
+	
+	@RequestMapping("/login")
+	public String welcome(Map<String, Object> model) {
+		model.put("message", this.message);
+		return "welcome";
+	}
+}
